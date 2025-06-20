0e4b199626ba0943e51334341b0e937d18ec2c0c
armmani <arm.maniii@gmail.com>
2025-06-20 19:34:57 +0700

    สร้าง error.middleware.js แล้วย้าย error จากหน้า server.js ไป

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
+import errorMiddleware from "./src/middlewares/error.middleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res
-    .status(err.statusCode || 500)
-    .json({ message: err.message } || "internal server error");
+app.use((req, res) => {
+  res.status(404).json({ message: `Not Found path: ${req.method} ${req.url}` });
 });
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 8877;
 
