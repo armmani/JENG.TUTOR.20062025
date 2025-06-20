1b93ce74af8b08f6abb14702a1c5ef5ee366857b
armmani <arm.maniii@gmail.com>
2025-06-20 20:13:24 +0700

    hash created

--- src/services/hash.service.js
+++ src/services/hash.service.js
@@ -1,1 +1,14 @@
-const hashService = {}
+import bcrypt from "bcryptjs";
+
+const hashService = {};
+
+hashService.hashPassword = (password) => {
+  return  bcrypt.hashSync(password, 10)
+}
+
+
+hashService.comparePassword = (password, hash) => {
+  return  bcrypt.comparePassword(password, hash)
+}
+
+export default hashService
