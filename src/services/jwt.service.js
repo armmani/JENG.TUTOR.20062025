c3e8b51f437c8a0c071ea3e0a18faf0ec8bd81ea
armmani <arm.maniii@gmail.com>
2025-06-20 20:48:09 +0700

    จบ jwt.service

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,9 +1,16 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const jwtService = {};
 
 jwtService.genAccessToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET)
-}
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+    algorithm: "HS256",
+  });
+};
+
+jwtService.verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET, { algorithms: ["HS256"] });
+};
 
 export default jwtService;
