be8752ae13efe8825200b08499e7d3a62eb65c37
armmani <arm.maniii@gmail.com>
2025-06-20 19:25:16 +0700

    กำหนด error (app.use(err..

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
 
-app.listen(PORT, () => console.log(`Server run on PORT: ${PORT}`))
+app.use(express.json());
+app.use(cors());
+
+app.use((err, req, res, next));
+
+const PORT = process.env.PORT || 8877;
+
+app.listen(PORT, () => console.log(`Server run on PORT: ${PORT}`));
